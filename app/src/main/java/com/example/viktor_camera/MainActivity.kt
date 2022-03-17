610226250ac8da2b045d896c5980ea94782739c2
Anbyeonghun <abh0920one@gmail.com>
2022-03-17 22:48:31 +0900

    Modify fragment replace 코드 추가

--- app/src/main/java/com/example/viktor_camera/MainActivity.kt
+++ app/src/main/java/com/example/viktor_camera/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.viktor_camera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        replace(CameraFragment())
+    }
+    fun replace(fragment: Fragment) {
+        val supportFragmentManager = supportFragmentManager
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
 }
