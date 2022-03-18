1d36619fd3a51b681352a8672fb6426d95fb72c2
Anbyeonghun <abh0920one@gmail.com>
2022-03-18 20:27:10 +0900

    Style 정보 입력 DetailActivity 퍼블리싱

---
+++ app/src/main/java/com/example/viktor_camera/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.viktor_camera
+
+import android.os.Bundle
+import com.example.viktor_camera.databinding.ActivityDetailBinding
+import com.foreveryone.knowing.base.BaseActivity
+
+class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
