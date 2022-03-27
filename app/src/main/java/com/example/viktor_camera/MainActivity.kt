59b16dce2a5beb84804b52be64572d2bf6583f1e
Anbyeonghun <abh0920one@gmail.com>
2022-03-27 21:49:11 +0900

    [Modify] intent 메소드화

--- app/src/main/java/com/example/viktor_camera/MainActivity.kt
+++ app/src/main/java/com/example/viktor_camera/MainActivity.kt
@@ -40,6 +40,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             if (it.resultCode == Activity.RESULT_OK) {
                 val data = it.data?.data!!.path
                 val uri = it.data?.data!!
+                intent(uri.toString())
             }
         }
 
@@ -96,6 +97,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                 isRecording = false
                 videoCapture.stopRecording()
             }
+        }
+        binding.galleryBtn.setOnClickListener{
+            GalleryHelper(this).selectPhoto(gallerySelect)
         }
     }
 
@@ -172,12 +176,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                         ).show()
                         Log.d("123","저장됨 outputFileResults.savedUri: ${outputFileResults.savedUri}")
                         Log.d("123", "저장됨 ${Uri.fromFile(File(outputFileResults.savedUri.toString()))}")
-                        val intent = Intent(this@MainActivity, DetailActivity::class.java)
-                        intent.apply {
-                            putExtra("uri", outputFileResults.savedUri.toString())
-                        }
-
-                        startActivity(intent)
+                        intent(outputFileResults.savedUri.toString())
                     }
 
                     override fun onError(
@@ -224,5 +223,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         return uri
     }
 
-
+    private fun intent(uri: String) {
+        val intent = Intent(this@MainActivity, DetailActivity::class.java)
+        intent.apply {
+            putExtra("uri", uri)
+        }
+        startActivity(intent)
+    }
 }
